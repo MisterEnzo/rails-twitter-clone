9194af13ded07c8924909a450009cc66fbce6cd8
Enzo Naguiat <enzonaguiat@protonmail.com>
2018-02-20 07:27:06 +0000

    fixes the test for unsuccessful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,10 +9,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name: "Dave", 
+    patch user_path(@user), params: { user: { name: "", 
                                          email:"Mat@o.",
                                          password:             "asdf",
                                          password_confirmation:"zxcv"} }
     assert_template 'users/edit'
+    assert_select 'div.alert', "The form contains 4 errors"
   end
 end
