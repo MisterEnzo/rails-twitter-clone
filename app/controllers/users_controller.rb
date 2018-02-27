03148d89eee63ca5aebdea4080f4f685a9887892
Enzo Naguiat <enzonaguiat@protonmail.com>
2018-02-27 07:04:26 +0000

    adds activation columns in the User model, adds activation-related methods in user.rb, updates seeds and fixtures for activation attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,4 +70,5 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
@@ -37,4 +38,16 @@ class User < ApplicationRecord
     return false if self.remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  private
+    # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
+    
+    # Create and assign and activation token and digest to the new user
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
