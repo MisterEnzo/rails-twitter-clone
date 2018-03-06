b9b796338ac260af084bbed195b4f2c6452e0a76
Enzo Naguiat <enzonaguiat@protonmail.com>
2018-03-06 09:06:35 +0000

    adds password reset and related tests

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,16 +1,18 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # https://learn-enough-rails-tutorial-enriquelorenzo.c9users.io/rails/mailers/user_mailer/account_activation.txt
   def account_activation
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at
+  # https://learn-enough-rails-tutorial-enriquelorenzo.c9users.io/rails/mailers/user_mailer/password_reset.txt
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
